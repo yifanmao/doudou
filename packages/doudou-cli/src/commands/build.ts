5119ca90a7176edbb062e17f12262e9ec9822fc1
Jason <yifan.na.mao@gmail.com>
2021-08-11 23:27:10 +0800

    使用项目环境taro编译

--- packages/doudou-cli/src/commands/build.ts
+++ packages/doudou-cli/src/commands/build.ts
@@ -2,6 +2,6 @@ import * as shelljs from 'shelljs'
 
 export default function build (args: any) {
   const { type = 'weapp', watch, env = 'development' } = args
-  let command = `NODE_ENV=${env} taro build --type ${type}${watch ? ' --watch' : ''}`
+  let command = `NODE_ENV=${env} ./node_modules/.bin/taro build --type ${type}${watch ? ' --watch' : ''}`
   shelljs.exec(command)
 }
